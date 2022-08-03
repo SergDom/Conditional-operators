925023181eb2e83400709f51b642a448d2e817a9
sergeydomanov <nomad-labs@yandex.ru>
2022-08-03 20:08:19 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Задача 1");
+        int clientOs = 1;
+        if (clientOs  == 1) {
+            System.out.println ("Установите версию приложения для iOS по ссылке");
+        } else {
+            System.out.println("Установите версию приложения для Android по ссылке");
+        }
+
+
+
+    }
+}
