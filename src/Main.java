ca88601aff9968b1022fce6d4392daf83c7a7a4c
sergeydomanov <nomad-labs@yandex.ru>
2022-08-03 21:48:20 +0300

    first commit

--- src/Main.java
+++ src/Main.java
@@ -12,19 +12,18 @@ public class Main {
 
         System.out.println("Задача 2");
 
-        int clientSystemIos = 0;
-        int clientSystemAndroid = 1;
+        int clientSystem = 1;
         // iOS - 0, Android =1
         int clientDeviceYear = 2016;
         // before 2015 - light version
 
-        if (clientSystemIos == 0 && clientDeviceYear < 2015 ) {
+        if (clientSystem == 0 && clientDeviceYear < 2015 ) {
             System.out.println("Установите облегченную версию iOS ");
-        } else if (clientSystemIos == 0 && clientDeviceYear >= 2015) {
+        } else if (clientSystem == 0 && clientDeviceYear >= 2015) {
             System.out.println("Установите полную версию iOS ");}
-        if (clientSystemIos == 1 && clientDeviceYear < 2015 ) {
+        if (clientSystem == 1 && clientDeviceYear < 2015 ) {
             System.out.println("Установите облегченную версию Android ");
-        } else if (clientSystemIos == 1 && clientDeviceYear >= 2015) {
+        } else if (clientSystem == 1 && clientDeviceYear >= 2015) {
             System.out.println("Установите полную версию Android ");}
 
         }
